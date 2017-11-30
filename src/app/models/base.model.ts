31a81e41faca6d8833de0bb84935fa655077bf8a
Swami Naik <swami@automatapi.com>
2017-11-30 18:56:06 +0530

    Changes in BaseModel

--- src/app/models/base.model.ts
+++ src/app/models/base.model.ts
@@ -2,12 +2,10 @@ export class BaseModel {
   _id: string;
   statusCd: string;
   companyId: string;
-  operationType: string;
 
   constructor() {
     this._id = null;
     this.statusCd = null;
     this.companyId = null;
-    this.operationType = null;
   }
 }
