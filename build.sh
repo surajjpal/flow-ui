cfaa1db8cab8bc4ccb2907ff0e4b79d8f1f93890
manjeet <manjeet@automatapi.com>
2018-02-12 22:25:13 +0530

    updated build script

--- build.sh
+++ build.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 _tag=$1
 
-if [ -z "${_tag}" ]; then
-    source _VERSION   
-    _tag=${_VERSION}
-fi
+#if [ -z "${_tag}" ]; then
+#    source _VERSION   
+#    _tag=${_VERSION}
+#fi
 
-docker build --tag "api-flow:${_tag}"  --no-cache=true .
+#docker build --tag "api-flow:${_tag}"  --no-cache=true .
+
+
+# AOT Compilation
+ng build --env=prod --aot --build-optimizer --base-href / --deploy-url /
+
+rm -rf target
+mkdir target
+zip -r target/flow-ui.zip dist/*
+
