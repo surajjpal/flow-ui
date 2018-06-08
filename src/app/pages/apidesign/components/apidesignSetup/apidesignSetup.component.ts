4736f2ed50845dea757d2ad7f7fe726f058a4005
sanket <sanket@automatapi.com>
2018-06-08 19:52:05 +0530

    initaite setup screen

--- src/app/pages/apidesign/components/apidesignSetup/apidesignSetup.component.ts
+++ src/app/pages/apidesign/components/apidesignSetup/apidesignSetup.component.ts
@@ -10,6 +10,8 @@ import { Subscription } from 'rxjs/Subscription';
 import { AlertService, DataSharingService } from '../../../../services/shared.service';
 import { ApiDesignService } from '../../../../services/apidesign.service'
 import { environment } from '../../../../../environments/environment';
+import { error } from 'selenium-webdriver';
+import { Algorithm, BusinessObject, BusinessObjectAlgorithm } from '../../../../models/businessobject.model';
 
 
 @Component({
@@ -19,11 +21,25 @@ import { environment } from '../../../../../environments/environment';
 export class ApiDesignSetupComponent implements OnInit, OnDestroy {
     apiDesignCreateMode: boolean;
     private subscription: Subscription;
+    private definedAlgorithms: Algorithm[];
+    private selectedBusinessObject: BusinessObject;
+    tempAlgorithm: BusinessObjectAlgorithm;
+
+    businessObjectUpdateCreateMsg = '';
+    supportedAlgorithmCategory = ["CLASSIFIER", "REGRESSION"];
 
     constructor(
-          private apiDesignService: ApiDesignService
+          private apiDesignService: ApiDesignService,
+          private router: Router,
+          private route: ActivatedRoute,
+          private alertService: AlertService,
+          private sharingService: DataSharingService
         ) {
         this.apiDesignCreateMode = true;
+        this.definedAlgorithms = [];
+        this.selectedBusinessObject = new BusinessObject();
+        this.businessObjectUpdateCreateMsg = '';
+        this.tempAlgorithm = new BusinessObjectAlgorithm();
     }
 
   ngOnInit() {
@@ -32,13 +48,38 @@ export class ApiDesignSetupComponent implements OnInit, OnDestroy {
     .subscribe(
       response => {
         console.log(response);
+        this.definedAlgorithms = response;
+      }, error => {
+        this.definedAlgorithms = [];
       }
     );
+    const businessObject: BusinessObject = this.sharingService.getSharedObject();
+    if (businessObject) {
+      this.selectedBusinessObject = businessObject;
+      this.apiDesignCreateMode = false;
+    }
+    else {
+      this.selectedBusinessObject = new BusinessObject();
+      this.apiDesignCreateMode = true;
+    }
+
   }
 
   ngOnDestroy(): void {
     console.log("apidesign setup on destroy");
   }
 
+  createBusinesObject() {
+    this.businessObjectUpdateCreateMsg = "business object created successfully";
+  }
+
+  onAlgorithmAdd() {
+    if (this.selectedBusinessObject && this.selectedBusinessObject.algorithms) {
+      this.selectedBusinessObject.algorithms.push(new BusinessObjectAlgorithm());
+    }
+    else{
+      this.selectedBusinessObject.algorithms = [];
+    }
+  }
   
 }
--- src/app/services/apidesign.service.ts
+++ src/app/services/apidesign.service.ts
@@ -7,23 +7,33 @@ import { Subject } from "rxjs/Subject";
 import { environment } from "environments/environment.prod";
 import { HttpClient } from "@angular/common/http";
 import { HttpResponse, HttpErrorResponse } from "@angular/common/http";
+import { UniversalUser } from './shared.service'
 
 @Injectable()
 export class ApiDesignService {
 
-    private httpHeaders = new HttpHeaders({ 'Content-Type' : 'application/json' });
+    private httpHeaders = new HttpHeaders();
 
-    constructor(private httpClient: HttpClient) { }
+    constructor(private httpClient: HttpClient, private univaersalUser: UniversalUser) { }
 
     getAlgorithms():Observable<Algorithm[]> {
         const subject = new Subject<Algorithm[]>();
-        
-
-        const url = `${environment.apiDesignUrl + environment.algorithmUrl}`
-
+        const companyId = this.univaersalUser.getUser().companyId;
+        const headers = new HttpHeaders({'Content-Type' : 'application/json', 'x-customer-id' : companyId});
+        const url = `${environment.apiDesignUrl + environment.algorithmUrl}`;
+        console.log("companyID");
+        console.log(companyId);
+        //this.httpHeaders.append('Content-Type', 'application/json');
+        //this.httpHeaders.append('x-customer-id', companyId);
+        // this.httpHeaders.set('x-customer-id', companyId);
+        // this.httpHeaders.set('Access-Control-Allow-Credentials', "true");
+        // this.httpHeaders.set('Access-Control-Allow-Origin', "*");
+        console.log("request headers");
+        console.log(headers);
         this.httpClient.get<Algorithm[]>(
             url,
             {
+                headers: headers,
                 observe: 'response',
                 reportProgress: true,
                 withCredentials: true
