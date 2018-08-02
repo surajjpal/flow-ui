df8349d831a04a8562060ee38397e76cabadf543
Swami Naik <swami@automatapi.com>
2018-08-02 21:29:41 +0530

    Removed analytics as a module and merged its component to setup(master) module. Added missing api in environment.demo.ts. Some minor bug fixes.

--- src/app/pages/analytics/analytics.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'api-analytics',
-  template: `<router-outlet></router-outlet>`
-})
-export class AnalyticsComponent {
-    constructor() {}
-}
