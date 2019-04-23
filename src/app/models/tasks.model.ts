9f501a1fd582650d680a4ddae27a18edb0f91339
Devesh Rao <devesh@automatapi.com>
2019-04-24 01:45:28 +0530

    Timeline

--- src/app/models/tasks.model.ts
+++ src/app/models/tasks.model.ts
@@ -6,17 +6,17 @@ export class Chronology {
 }
 
 
-export class EmailPersister{
-  to:string;
-  from:string;
-  subject:string;
-  cc:string;
-  htmlText:any;
-  parsedBody:string;
-  entityId:string;
-  fileUrls:any;
-  emailType:string;
-  dataModel:any;
+export class EmailPersister {
+  to: string;
+  from: string;
+  subject: string;
+  cc: string;
+  htmlText: any;
+  parsedBody: string;
+  entityId: string;
+  fileUrls: any;
+  emailType: string;
+  dataModel: any;
 }
 
 export class Time {
@@ -33,7 +33,7 @@ export class Time {
   chronology: Chronology;
 }
 
-export class Amount{
+export class Amount {
   typecd: string;
   amount: number;
   currency: string;
@@ -46,10 +46,10 @@ export class State extends BaseModel {
   stateCd: string;
   stageCd: string;
   stateMachineInstanceModelId: string;
-  assignedUserDisplayName:string;
+  assignedUserDisplayName: string;
   assignedUserGroupCd: string;
   assignedUserId: string;
-  flagReason:string;
+  flagReason: string;
   assignedUserName: string;
   startTime: Time;
   endTime: Time;
@@ -57,16 +57,16 @@ export class State extends BaseModel {
   reservedTime: Time;
   payload: string;
   fromStateCd: string;
-  parameters: Map<string,string>;
-  predictedParameters: Map<string,string>;
+  parameters: Map<string, string>;
+  predictedParameters: Map<string, string>;
   businessCost: Amount;
   errorMessageMap: any;
   headerParamList: string[];
-  flagged:boolean;
-  iterationLevel:number;
-  autoAllocation:boolean;
-  assignedDueToFlagged:boolean;
-  
+  flagged: boolean;
+  iterationLevel: number;
+  autoAllocation: boolean;
+  assignedDueToFlagged: boolean;
+
 }
 
 export class CommonInsightWrapper extends BaseModel {
@@ -85,19 +85,35 @@ export class StateReportModel extends BaseModel {
   pendingStatusCount: number;
   assignedUserName: string;
   assignedUserId: string;
-  closedStatus:number;
+  closedStatus: number;
   fromDate: string;
   toDate: string;
-  frDt:any;
+  frDt: any;
   toDt: any;
   bussinessKeyMobile: string;
   reportStartTime: string;
-  reportEndTime:string;
-  reportStatusCd:string;
-  reportSubStatusCd:string;
-  tatTime:any;
-  reportTatTime:string;
-  chatType:string;
-  businessKeyImeiEmail:string;
-  assignedList:string[] = [];
+  reportEndTime: string;
+  reportStatusCd: string;
+  reportSubStatusCd: string;
+  tatTime: any;
+  reportTatTime: string;
+  chatType: string;
+  businessKeyImeiEmail: string;
+  assignedList: string[] = [];
+}
+
+export class TimelineStateAuditData extends BaseModel {
+  stateCd: string;
+  assignedUserGroupCd: string;
+  assignedUserName: string;
+  assignedUserDisplayName: string;
+  assignedUserId: string;
+  startTime: Time;
+  endTime: Time;
+  parameters: TimelineStateParameterData[];
+}
+
+export class TimelineStateParameterData extends BaseModel {
+  key: string;
+  value: string;
 }
